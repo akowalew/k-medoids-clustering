187bf6620ec94406b1d4cb54d3cb9f9d3244ff23
akowalew <ram.techen@gmail.com>
2017-05-18 02:06:41 +0200

    ADD: K-Medoids full loop

--- main.R
+++ main.R
@@ -30,6 +30,17 @@ generatePopulation <- function(popSize) {
 	return(population)
 }
 
+# sum of all distances to medoid 
+# of elements inside cluster
+calculateCost <- function(medoid, cluster) {
+	totalDistance <- 0
+	for(elem in cluster) {
+		distance <- dist(rbind(elem, medoid), method=distMethod)
+		totalDistance <- totalDistance + distance
+	}
+	return(totalDistance)
+}
+
 popSize <- 50
 population <- generatePopulation(popSize)
 
@@ -77,54 +88,87 @@ for(idx in 1:(popSize - clustersSize)) {
 	clusters[[minDistanceIdx]] <- c(cluster, list(elem))
 }
 
-# sum of all distances to medoid 
-# of elements inside cluster
-calculateCost <- function(medoid, cluster) {
-	totalDistance <- 0
-	for(elem in cluster) {
-		distance <- dist(rbind(elem, medoid), method=distMethod)
-		totalDistance <- totalDistance + distance
-	}
-	return(totalDistance)
-}
+while(1) {
+	# for each cluster find better medoid
+	wasChanged <- FALSE
+	for(i in 1:clustersSize) {
+		cluster <- clusters[[i]]
 
-# for each cluster find better medoid
-wasChanged <- FALSE
-for(i in 1:clustersSize) {
-	cluster <- clusters[[i]]
-	medoid <- medoids[[i]]
-	currentCost <- calculateCost(medoid, cluster)
-
-	# by replacing each cluster elem with medoid
-	# find the lowest cost from all configurations
-	minCost <- Inf
-	minCostIdx <- -1
-	for(j in 1:length(cluster)) {
-		# swap medoid with j-th cluster's elem
-		newMedoid <- cluster[[j]]
-		cluster[[j]] <- medoid
-		medoid <- newMedoid
-
-		# check, if the cost was reduced
-		cost <- calculateCost(medoid, cluster)
-		if(cost < minCost) {
-			minCost <- cost
-			minCostIdx <- j
+		# if cluster is empty, nothing to find
+		if(length(cluster) == 0) {
+			next 
 		}
 
-		# undo the swap
-		oldMedoid <- cluster[[j]]
-		cluster[[j]] <- medoid
-		medoid <- oldMedoid
+		medoid <- medoids[[i]]
+		currentCost <- calculateCost(medoid, cluster)
+
+		# by replacing each cluster elem with medoid
+		# find the lowest cost from all configurations
+		minCost <- Inf
+		minCostIdx <- -1
+		for(j in 1:length(cluster)) {
+			# swap medoid with j-th cluster's elem
+			newMedoid <- cluster[[j]]
+			cluster[[j]] <- medoid
+			medoid <- newMedoid
+
+			# check, if the cost was reduced
+			cost <- calculateCost(medoid, cluster)
+			if(cost < minCost) {
+				minCost <- cost
+				minCostIdx <- j
+			}
+
+			# undo the swap
+			oldMedoid <- cluster[[j]]
+			cluster[[j]] <- medoid
+			medoid <- oldMedoid
+		}
+
+		# if best found configuration is better than current
+		if(minCost < currentCost) {
+			# swap best elem with current medoid
+			bestElem <- cluster[[minCostIdx]]
+			cluster[[minCostIdx]] <- medoids[[i]]
+			medoids[[i]] <- bestElem
+			clusters[[i]] <- cluster
+			wasChanged <- TRUE
+		}
 	}
 
-	# if best found configuration is better than current
-	if(minCost < currentCost) {
-		# swap best elem with current medoid
-		bestElem <- cluster[[minCostIdx]]
-		cluster[[minCostIdx]] <- medoids[[i]]
-		medoids[[i]] <- bestElem
-		clusters[[i]] <- cluster
-		wasChanged <- TRUE
+	# check, if elements can be assigned to other medoids
+	# but if there was no changes inside clusters, end algorithm
+	if(!wasChanged)
+		break
+
+	# unpack all elements
+	population <- vector("list", restPopSize)
+	idx <- 1
+	for(k in 1:clustersSize) {
+		cluster <- clusters[[k]]
+		if(length(cluster) != 0) {
+			population[idx:(length(cluster)+idx-1)] <- cluster
+			idx <- idx + length(cluster)
+			clusters[[k]] <- list()
+		}
+	}
+
+	# assign elements of population into clusters
+	for(elem in population) {
+		# find the nearest medoid
+		minDistance <- Inf
+		minDistanceIdx <- -1
+		for(i in 1:clustersSize) {
+			medoid <- medoids[[i]]
+			distance <- dist(rbind(elem, medoid), method=distMethod)
+			if(distance < minDistance) {
+				minDistance <- distance
+				minDistanceIdx <- i
+			}
+		}
+
+		# push elem to nearest cluster
+		cluster <- clusters[[minDistanceIdx]]
+		clusters[[minDistanceIdx]] <- c(cluster, list(elem))
 	}
 }
